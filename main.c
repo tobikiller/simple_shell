78695fd6b05551fec85a2160d5863a85ca05b51c
Tobikiller <edwardnguimeya@yahoo.com>
2022-11-16 13:11:58 -0800

    adjusment and fixup

--- main.c
+++ main.c
@@ -1,44 +1,57 @@
-#include "simpleshell.h"
-
+#include "shell.h"
 
 /**
- * main - creates a prompt that reads input, sparses it, executes and waits
- * for another command unless told to exit
- * @ac: number of arguemnets
- * @av: array of arguements
- * @env: environment variable
- * Return: EXIT_SUCCESS
+ * main - the main function that starts the shell
+ *			and calls the shell_loop function
+ *
+ * @argc: arguement count passed to main attribute unused
+ * @argv: character array passed to the main function
+ *
+ * Return: 1 on success
  */
-int main(int ac __attribute__((unused)), char **av, char **env)
+int main(int argc __attribute__((unused)), char **argv)
 {
-	char *line;
-	char **args, **path;
-	int count = 0, status = 0;
-	(void) av;
+	shell_t var;
 
-	signal(SIGINT, handle_signal);
-	while (1)
-	{
-		prompt();
-		/*read input and return string*/
-		line = read_input();
-		/*separates string to get command and atgs*/
+	shell_init(&var);
+	var.shell_name = argv[0];
 
-		args = sparse_str(line, env);
-
-		if ((_strcmp(args[0], "\n") != 0) && (_strcmp(args[0], "env") != 0))
-		{
-			count += 1;
-			path = search_path(env);
-			status = _stat(args, path);
-			child_process(av, args, env, status, count);
-		}
-		else
-		{
-			free(args);
-		}
-		free(line);
-	}
+	/* call the loop function */
+	shell_loop(&var);
+	free_tokenized(environ);
+	free_tokenized(var.aliases);
 	return (EXIT_SUCCESS);
+}
 
+/**
+ * shell_init - some initialisations
+ *
+ * @var: shell global variable
+ * Return: shell var
+ */
+shell_t *shell_init(shell_t *var)
+{
+	int i;
+	char **tmp;
+
+	var->shell_name = NULL;
+	var->old_pwd = NULL;
+	var->err_status = 0;
+	var->cmd_counter = 1;
+
+	for (i = 0; environ[i]; i++)
+		;
+
+	tmp = malloc(sizeof(char *) * (i + 1));
+	var->aliases = malloc(sizeof(char *));
+	var->aliases[0] = NULL;
+
+	for (i = 0; environ[i]; i++)
+	{
+		tmp[i] = _strdup(environ[i]);
+	}
+	tmp[i] = NULL;
+	environ = tmp;
+
+	return (var);
 }
