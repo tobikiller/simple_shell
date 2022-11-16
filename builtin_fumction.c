905d7c704ba4bd383282f9f2db4d6d447ed6e3ac
Tobikiller <edwardnguimeya@yahoo.com>
2022-11-16 14:55:50 -0800

    betty fix

--- builtin_fumction.c
+++ builtin_fumction.c
@@ -57,6 +57,7 @@ void quit(char **tokenized_command, shell_t *p)
 	else
 		_printf("$: exit doesn't take more than one argument\n", STDERR_FILENO);
 }
+
 #define SETPWD(V) (V = _strdup(_getenv("OLDPWD")))
 
 /**
@@ -70,7 +71,6 @@ void ch_dir(char **command, shell_t *var)
 	char *home;
 
 	home = _getenv("HOME");
-
 	if (command[1] == NULL)
 	{
 		SETPWD(var->old_pwd);
@@ -109,28 +109,19 @@ void ch_dir(char **command, shell_t *var)
 	}
 	var->err_status = 0;
 }
+
 #undef GETCWD
 /**
  * display_help - display help necessary help and docs for the shell
  * @command: command passed
  * @p: shell global variable
  */
+
 void display_help(char **command __attribute__((unused)),
 		shell_t *p __attribute__((unused)))
 {
 	static char *texts[] = {
 		"\n\t***Welcome to Our Simple Shell***\n",
-		"\nList of Supported Commands:\n",
-		"\tcd\n",
-		"\trmdir\n",
-		"\tmkdir\n",
-		"\tsetenv\n",
-		"\tunsetenv\n",
-		"\thelp\n",
-		"\tclear\n",
-		"\texit\n",
-		"\nAll Other Shell Built in Commands are supported\n",
-		"\nHave Fun!!\n",
 		NULL
 		};
 	int i = 0;
