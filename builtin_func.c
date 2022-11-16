30bd6a9ff75aaea5c2407a32bdd0e3808d1c9a14
Tobikiller <edwardnguimeya@yahoo.com>
2022-11-16 07:26:46 -0800

    implementing part one shell by tobi

---
+++ builtin_func.c
@@ -0,0 +1,14 @@
+#include "simpleshell.h"
+
+/**
+ * func_printenv - prints the current environment
+ * @environ: environment variable
+ */
+void func_printenv(char **environ)
+{
+
+	int i = 0;
+
+	for (; environ[i] ; i++)
+		_puts(environ[i]); /*printing environment */
+}
---
+++ main.c
@@ -0,0 +1,44 @@
+#include "simpleshell.h"
+
+
+/**
+ * main - creates a prompt that reads input, sparses it, executes and waits
+ * for another command unless told to exit
+ * @ac: number of arguemnets
+ * @av: array of arguements
+ * @env: environment variable
+ * Return: EXIT_SUCCESS
+ */
+int main(int ac __attribute__((unused)), char **av, char **env)
+{
+	char *line;
+	char **args, **path;
+	int count = 0, status = 0;
+	(void) av;
+
+	signal(SIGINT, handle_signal);
+	while (1)
+	{
+		prompt();
+		/*read input and return string*/
+		line = read_input();
+		/*separates string to get command and atgs*/
+
+		args = sparse_str(line, env);
+
+		if ((_strcmp(args[0], "\n") != 0) && (_strcmp(args[0], "env") != 0))
+		{
+			count += 1;
+			path = search_path(env);
+			status = _stat(args, path);
+			child_process(av, args, env, status, count);
+		}
+		else
+		{
+			free(args);
+		}
+		free(line);
+	}
+	return (EXIT_SUCCESS);
+
+}
